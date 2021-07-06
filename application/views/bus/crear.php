4fbac508d271752c74efec2f4c45638494dd76f8
rosa clara <rosa.huanaco.apaza@gmail.com>
2021-07-05 23:56:52 -0400

    modificando el formulario

--- application/views/bus/crear.php
+++ application/views/bus/crear.php
@@ -24,7 +24,7 @@
           <div class="col-12">
             <div class="card">
               <div class="card-header">                
-                <h3 class="card-title">FORMULARIO DE REGISTRO DE UNA NUEVA FLOTA</h3>
+                <h3 class="card-title">formulario Registr de Buses</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
